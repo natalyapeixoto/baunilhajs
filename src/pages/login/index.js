805d506d27ba0dfc2231bc3aa405207f345236bc
Natalya <natalya_peixoto@hotmail.com>
2018-12-17 14:16:03 -0200

    typo correction and yellow bg forum page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -5,15 +5,12 @@
 
   function signUpClick(e){
     e.preventDefault()
-    console.log(e)
-    const loadingGif = document.getElementById('loading-gif-login')
     const nome = document.getElementById('nome')
     const email = document.getElementById('cadastro-email')
     const password = document.getElementById('cadastro-password')
     const nomeArg = nome.value
     const emailArg = email.value
     const passArg = password.value
-    loadingGif.innerHTML = `<img id="loading" src="loading.gif">`
     registerNewUser(nomeArg, emailArg, passArg)
   }
 
@@ -38,8 +35,6 @@
       const email = document.getElementById('login-email').value
       const password = document.getElementById('login-password').value
       console.log(email)
-      const loadingGif = document.getElementById('loading-gif-cadastro')
-      loadingGif.innerHTML = `<img  src="../../loading.gif">`
       singInUser(email, password)
      
   }
